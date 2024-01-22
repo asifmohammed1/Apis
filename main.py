ec3463f7e7a7459519e88382e4d0f643b1836618
Asif Mohammed <82757214+sonuasif748@users.noreply.github.com>
2024-01-22 19:33:40 +0530

    3

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from selenium.webdriver.chrome.options import Options
 import requests
 import json
 from fastapi.middleware.cors import CORSMiddleware
-import todo
+from todo import *
 # from requests_html import HTTPSession
 # from webdriver_manager.chrome import ChromeDriverManager
 
